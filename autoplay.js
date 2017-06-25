61d763334454ab2fbdf30bb512a41d699f06c0c9
Duke <emersonalmeidax@gmail.com>
2017-06-25 12:04:17 -0300

    improve load

--- autoplay.js
+++ autoplay.js
@@ -1,9 +1,10 @@
-window.onload = function() {
+(function(){
   var audio = document.querySelector('audio');
+
   audio && audio.addEventListener('abort', function() {
     var episodes = document.querySelectorAll('.episode_row:not(.episode_played) .episode_button:not(.playing)');
     var nextEpisode = episodes[0];
     console.log("playing next episode");
     nextEpisode.click();
   });
-};
+}());
