c3462c5884373727c8dc99b06348ee5147050d5f
Duke <emersonalmeidax@gmail.com>
2017-06-25 11:52:51 -0300

    initial commit

---
+++ autoplay.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  var audio = document.querySelector('audio');
+  audio && audio.addEventListener('abort', function() {
+    var episodes = document.querySelectorAll('.episode_row:not(.episode_played) .episode_button:not(.playing)');
+    var nextEpisode = episodes[0];
+    console.log("playing next episode");
+    nextEpisode.click();
+  });
+};
